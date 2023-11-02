aec7fc54a7d0110b9633b036aafbb5f3f420985e
piet <pprotassow@posteo.de>
2023-11-02 17:37:20 +0100

    add test

--- apis/tests.py
+++ apis/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from books.models import Book
 
-# Create your tests here.
+class APITests(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.book = Book.objects.create(title='Test Book', subtitle='Test Subtitle', author='Test', isbn='1234567890123') 
+
+    def test_api_listview(self):
+        url = reverse('book_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Book.objects.count(), 1)
+        self.assertContains(response, self.book)
+
+
